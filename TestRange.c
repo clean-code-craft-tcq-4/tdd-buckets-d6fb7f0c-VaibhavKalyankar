1e1b46cfe45cb7bddab7938ba84dcd435d380766
VaibhavKalyankar <105204227+VaibhavKalyankar@users.noreply.github.com>
2022-08-21 15:32:04 +0530

    Update TestRange.c

--- TestRange.c
+++ TestRange.c
@@ -1,1 +1,18 @@
+#include <assert.h>
+#include "RangeCapture.h"
+
+int main()
+{
+  int CurrentSamples[] = {3,3,5,4,10,11,12};
+  int CurrentSamplesSize = sizeof(CurrentSamples) / sizeof(CurrentSamples[0]);
+  
+  int *CheckRange;
+  
+  CheckRange = DetectRange(CurrentSamples,CurrentSamplesSize);
+  
+  assert(*(CheckRange+4)) == 3);
+  
+  return 0;
+}
+  
 
